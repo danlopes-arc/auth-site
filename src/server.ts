b2a9819f9882cc1619c12577e9859b30701f7c9c
Daniel <danlpsarcanjo@gmail.com>
2021-01-24 23:29:17 -0500

    Add client git submodule

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import passport from 'passport'
+import path from 'path'
 
 import apiRoute from './routes/api'
 import passportConfig from './config/passport'
 import keys, { missingKeys } from './config/keys'
 
 const main = async () => {
-
   if (missingKeys.length > 0) {
     return console.error(
       `[server][error] the following keys are missing: ${missingKeys.join(
@@ -32,11 +32,18 @@ const main = async () => {
 
   const app = express()
 
+  const rootDir = path.join(__dirname, '..')
+
+  app.use(express.static(path.join(rootDir, 'client', 'build')))
   app.use(express.json())
   app.use(passport.initialize())
 
   app.use('/api', apiRoute)
 
+  app.get('/*', (req, res) =>
+    res.sendFile(path.join(rootDir, 'client', 'build', 'index.html'))
+  )
+  
   const port = keys.port
 
   app.listen(port, () => {
